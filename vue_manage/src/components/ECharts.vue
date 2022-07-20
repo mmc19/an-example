6b108ff51e3b1db8df1cce65e72f2b91ef859773
mmc19 <mmc0131@163.com>
2022-07-21 01:01:13 +0800

    2022-7-21 1:00 组件封装

---
+++ vue_manage/src/components/ECharts.vue
@@ -0,0 +1,125 @@
+<template>
+  <div ref="echarts"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  props: {
+    isAxisChart: {
+      type: Boolean,
+      default: true,
+    },
+    chartData: {
+      type: Object,
+      defaul() {
+        return {
+          xData: [],
+          series: [],
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      axisOption: {
+        legend: {
+          textStyle: {
+            color: "#333",
+          },
+        },
+        grid: {
+          left: "20%",
+        },
+        tooltip: {
+          trigger: "axis",
+        },
+        xAxis: {
+          type: "category", //类目轴
+          data: [],
+          axisLine: {
+            lineStyle: {
+              color: "#17b3a3",
+            },
+          },
+          axisLabel: {
+            intervval: 0,
+            color: "#333",
+          },
+        },
+        yAxis: [
+          {
+            type: "value",
+            axisLine: {
+              lineStyle: {
+                color: "#17b3a3",
+              },
+            },
+          },
+        ],
+        color: [
+          "#2ec7c9",
+          "#b6a2de",
+          "#5ab1ef",
+          "#ffb980",
+          "#d87a80",
+          "#8d98b3",
+        ],
+        series: [],
+      },
+      nomalOption: {
+        tooltip: {
+          trigger: "item",
+        },
+        color: [
+          "#0f78f4",
+          "#dd536b",
+          "#9462e5",
+          "#a6a6a6",
+          "#e1bb22",
+          "#39c362",
+          "#3ed1cf",
+        ],
+        series: [],
+      },
+      echart: null,
+    };
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      //   immediate: true,
+      handler() {
+        this.initChart();
+      },
+    },
+  },
+  methods: {
+    initChart() {
+      this.initChartData();
+      if (this.echart) {
+        this.echart.setOption(this.options);
+      } else {
+        this.echart = echarts.init(this.$refs.echarts);
+        this.echart.setOption(this.options);
+      }
+    },
+    initChartData() {
+      if (this.isAxisChart) {
+        this.axisOption.xAxis.data = this.chartData.xData;
+        this.axisOption.series = this.chartData.series;
+      } else {
+        this.nomalOption.series = this.chartData.series;
+      }
+    },
+  },
+  computed: {
+    options() {
+      return this.isAxisChart ? this.axisOption : this.nomalOption;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- vue_manage/src/views/home/index.vue
+++ vue_manage/src/views/home/index.vue
@@ -45,14 +45,21 @@
         </el-card>
       </div>
       <el-card style="height: 280px">
-        <div style="height: 280px" ref="echarts"></div>
+        <!-- <div style="height: 280px" ref="echarts"></div> -->
+        <EChart :chartData="echartData.order" style="height: 280px" />
       </el-card>
       <div class="graph">
         <el-card style="height: 260px">
-          <div style="height: 240px" ref="userEcharts"></div>
+          <!-- <div style="height: 240px" ref="userEcharts"></div> -->
+          <EChart :chartData="echartData.user" style="height: 280px" />
         </el-card>
         <el-card style="height: 260px">
-          <div style="height: 240px" ref="videoEcharts"></div>
+          <!-- <div style="height: 240px" ref="videoEcharts"></div> -->
+          <EChart
+            :chartData="echartData.video"
+            :isAxisChart="false"
+            style="height: 280px"
+          />
         </el-card>
       </div>
     </el-col>
@@ -62,8 +69,12 @@
 <script>
 import { getData } from "../../api/data.js";
 import * as echarts from "echarts";
+import EChart from "../../components/ECharts.vue";
 export default {
   name: "home",
+  components: {
+    EChart,
+  },
   data() {
     return {
       userImg: require("../../assets/images/user.png"),
@@ -112,6 +123,19 @@ export default {
           color: "#5ab1ef",
         },
       ],
+      echartData: {
+        order: {
+          xData: [],
+          series: [],
+        },
+        user: {
+          xData: [],
+          series: [],
+        },
+        video: {
+          series: [],
+        },
+      },
     };
   },
   mounted() {
@@ -130,97 +154,120 @@ export default {
             type: "line",
           });
         });
-        const option = {
-          xAxis: {
-            data: xData,
-          },
-          yAxis: {},
-          legend: {
-            date: keyArray,
-          },
-          series,
-        };
-        const E = echarts.init(this.$refs.echarts);
-        E.setOption(option);
+        // const option = {
+        //   xAxis: {
+        //     data: xData,
+        //   },
+        //   yAxis: {},
+        //   legend: {
+        //     date: keyArray,
+        //   },
+        //   series,
+        // };
+        this.echartData.order.xData = xData;
+        this.echartData.order.series = series;
+        // const E = echarts.init(this.$refs.echarts);
+        // E.setOption(option);
 
         //用户图
-        const userOption = {
-          legend: {
-            //图例文字颜色
-            textStyle: {
-              color: "#333",
-            },
+        // const userOption = {
+        //   legend: {
+        //     //图例文字颜色
+        //     textStyle: {
+        //       color: "#333",
+        //     },
+        //   },
+        //   grid: {
+        //     left: "20%",
+        //   },
+        //   //提示框
+        //   tooltip: {
+        //     trigger: "axis",
+        //   },
+        //   xAxis: {
+        //     type: "category", //类目轴
+        //     data: data.userData.map((item) => item.date),
+        //     axisLine: {
+        //       lineStyle: {
+        //         color: "#17b3a3",
+        //       },
+        //     },
+        //     axisLabel: {
+        //       intervval: 0,
+        //       color: "#333",
+        //     },
+        //   },
+        //   yAxis: [
+        //     {
+        //       type: "value",
+        //       axisLine: {
+        //         lineStyle: {
+        //           color: "#17b3a3",
+        //         },
+        //       },
+        //     },
+        //   ],
+        //   color: ["#2ec7c9", "#b6a2de"],
+        //   series: [
+        //     {
+        //       name: "新增用户",
+        //       data: data.userData.map((item) => item.new),
+        //       type: "bar",
+        //     },
+        //     {
+        //       name: "活跃用户",
+        //       data: data.userData.map((item) => item.active),
+        //       type: "bar",
+        //     },
+        //   ],
+        // };
+
+        this.echartData.user.xData = data.userData.map((item) => item.date);
+        this.echartData.user.series = [
+          {
+            name: "新增用户",
+            data: data.userData.map((item) => item.new),
+            type: "bar",
           },
-          grid: {
-            left: "20%",
+          {
+            name: "活跃用户",
+            data: data.userData.map((item) => item.active),
+            type: "bar",
           },
-          //提示框
-          tooltip: {
-            trigger: "axis",
-          },
-          xAxis: {
-            type: "category", //类目轴
-            data: data.userData.map((item) => item.date),
-            axisLine: {
-              lineStyle: {
-                color: "#17b3a3",
-              },
-            },
-            axisLabel: {
-              intervval: 0,
-              color: "#333",
-            },
-          },
-          yAxis: [
-            {
-              type: "value",
-              axisLine: {
-                lineStyle: {
-                  color: "#17b3a3",
-                },
-              },
-            },
-          ],
-          color: ["#2ec7c9", "#b6a2de"],
-          series: [
-            {
-              name: "新增用户",
-              data: data.userData.map((item) => item.new),
-              type: "bar",
-            },
-            {
-              name: "活跃用户",
-              data: data.userData.map((item) => item.active),
-              type: "bar",
-            },
-          ],
-        };
-        const U = echarts.init(this.$refs.userEcharts);
-        U.setOption(userOption);
+        ];
+
+        // const U = echarts.init(this.$refs.userEcharts);
+        // U.setOption(userOption);
 
         //饼图
-        const videoOption = {
-          tooltip: {
-            trigger: "item",
+        // const videoOption = {
+        //   tooltip: {
+        //     trigger: "item",
+        //   },
+        //   color: [
+        //     "#0f78f4",
+        //     "#dd536b",
+        //     "#9462e5",
+        //     "#a6a6a6",
+        //     "#e1bb22",
+        //     "#39c362",
+        //     "#3ed1cf",
+        //   ],
+        //   series: [
+        //     {
+        //       data: data.videoData,
+        //       type: "pie",
+        //     },
+        //   ],
+        // };
+        // const V = echarts.init(this.$refs.videoEcharts);
+        // V.setOption(videoOption);
+        this.echartData.video.series = [
+          {
+            data: data.videoData,
+            type: "pie",
           },
-          color: [
-            "#0f78f4",
-            "#dd536b",
-            "#9462e5",
-            "#a6a6a6",
-            "#e1bb22",
-            "#39c362",
-            "#3ed1cf",
-          ],
-          series: [
-            {
-              data: data.videoData,
-              type: "pie",
-            },
-          ],
-        };
-        const V = echarts.init(this.$refs.videoEcharts);
-        V.setOption(videoOption);
+        ];
       }
       console.log(res);
     });
